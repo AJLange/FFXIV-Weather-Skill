e81a816885fc79e619ed58429131308c99f59353
Amanda Lange <litagemini@gmail.com>
2022-11-02 17:01:44 -0400

    putting error handlers back

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -106,7 +106,82 @@ class GetForecastIntentHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 
+class FallbackIntentHandler(AbstractRequestHandler):
+    """Handler for Fallback Intent.
+    AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
 
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In FallbackIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        speech = data[prompts.FALLBACK_MESSAGE]
+        reprompt = data[prompts.FALLBACK_REPROMPT]
+        handler_input.response_builder.speak(speech).ask(
+            reprompt)
+        return handler_input.response_builder.response
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, this is an error message! Custom error message!"
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
+        )
+
+
+
+# *****************************************************************************
+# These simple interceptors just log the incoming and outgoing request bodies to assist in debugging.
+
+class LoggingRequestInterceptor(AbstractRequestInterceptor):
+    def process(self, handler_input):
+        print("Request received: {}".format(handler_input.request_envelope.request))
+
+
+class LoggingResponseInterceptor(AbstractResponseInterceptor):
+    def process(self, handler_input, response):
+        print("Response generated: {}".format(response))
 
 
 sb = SkillBuilder()
@@ -117,5 +192,14 @@ sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(GetForecastIntentHandler())
 
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+
+# register exception handlers
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+# register interceptors
+sb.add_global_request_interceptor(LoggingRequestInterceptor())
+sb.add_global_response_interceptor(LoggingResponseInterceptor())
 
 lambda_handler = sb.lambda_handler()
