08a8aaccdc88cb35eb88b2e95e7a9e910ca5d210
Amanda Lange <litagemini@gmail.com>
2022-11-02 16:44:50 -0400

    put forecast handler back in

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -58,24 +58,53 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
 
 
-class GetDataApiHandler(AbstractRequestHandler):
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
 
-        return utils.isApiRequest(handler_input, 'GetDataApi')
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = "You just triggered " + intent_name + "."
 
-    def handle(self):
-        '''
-        Holding this in place for if I need it later.
-        '''
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            # .ask("add a reprompt if you want to keep the session open for the user to respond")
+            .response
+        )
 
-        response = {
-            'apiResponse': {
-                
-            }
-        }
 
-        return response
+class GetForecastIntentHandler(AbstractRequestHandler):
+    '''intent to get a weather forecast'''
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_request_type("LaunchRequest")(handler_input) or
+                is_intent_name("GetForecastIntent")(handler_input))
+
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In GetForecastIntentHandler")
+
+        # get localization data
+        data = handler_input.attributes_manager.request_attributes["_"]
+
+        my_forecast = random.choice(data[prompts.WEATHER])
+        speech = data[prompts.GET_WEATHER].format(my_forecast)
+
+        handler_input.response_builder.speak(speech).set_card(
+            SimpleCard(data[prompts.SKILL_NAME], my_forecast))
+        return handler_input.response_builder.response
+
 
 
 
@@ -83,9 +112,10 @@ class GetDataApiHandler(AbstractRequestHandler):
 sb = SkillBuilder()
 
 # register request / intent handlers
-sb.add_request_handler(GetDataApiHandler())
+
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(GetForecastIntentHandler())
 
 
 lambda_handler = sb.lambda_handler()
